4713000d4cf6f10fcc8283cc0cec07e99f074532
Joaquin Bas <74403807+LordMrMittens@users.noreply.github.com>
2022-02-04 11:35:48 +0000

    update 040222

--- Bit London/Assets/Scripts/PlayerClickMove.cs
+++ Bit London/Assets/Scripts/PlayerClickMove.cs
@@ -27,4 +27,9 @@ public class PlayerClickMove : MonoBehaviour
             }
         }
     }
+
+    public void OnMouseDown()
+    {
+        CameraController.instance.followTransform = transform;
+    }
 }
