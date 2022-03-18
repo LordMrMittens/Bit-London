97ff052c4bd75abf18cc18f9fad6a2850ecdcdcd
Fatih <fd1677n@gre.ac.uk>
2022-03-18 13:19:30 +0000

    Final

--- Bit London/Assets/Scripts/NPCMovement.cs
+++ Bit London/Assets/Scripts/NPCMovement.cs
@@ -9,28 +9,63 @@ public class NPCMovement : MonoBehaviour
     [SerializeField] int currentWaypoint = 0;
     NavMeshAgent agent;
     [SerializeField] float reachTargetRange = 2f;
+    [SerializeField] float speed;
+    [SerializeField] bool check;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (agent.remainingDistance < reachTargetRange)
+        /*if (agent.remainingDistance < reachTargetRange)
         {
             GoToNextWaypoint();
+        }*/
+        
+        if (!PauseMenu.gameIsPaused)
+        {
+            if (check)
+            {
+                GoToNextWaypoint();
+            }
+            else
+            {
+                if(agent == null)
+                {
+                    agent = GetComponent<NavMeshAgent>();
+                }
+                if (agent.remainingDistance < reachTargetRange)
+                {
+                    HumanPathFind();
+                }
+            }
         }
     }
 
     void GoToNextWaypoint()
     {
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].transform.position) < reachTargetRange)
+        {
+            currentWaypoint++;
+            if (currentWaypoint >= waypoints.Length)
+                currentWaypoint = 0;
+            //Destroy(gameObject);
+        }
+
+        //agent.SetDestination(waypoints[currentWaypoint].transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position, (speed / 100));
+        transform.LookAt(waypoints[currentWaypoint].transform.position);
+    }
+
+    void HumanPathFind()
+    {
+
         currentWaypoint++;
         if (currentWaypoint >= waypoints.Length)
             currentWaypoint = 0;
-            //Destroy(gameObject);
 
         agent.SetDestination(waypoints[currentWaypoint].transform.position);
     }
